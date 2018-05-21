98db96ea372816c78d96a6052a56d91c20fac2c2
gaols <igaols@foxmail.com>
2018-05-21 18:49:09 +0800

    concurrency utils

---
+++ co/coutils.go
@@ -0,0 +1,44 @@
+package co
+
+import "sync"
+
+type Job interface {
+	// Run run the current job
+	Run() (ret interface{}, err error)
+}
+
+// PoolJobs execute jobs concurrently with limited concurrency, you can stop on error or ignore errors and proceed on.
+func PoolJobs(jobs []Job, concurrency int, stopOnErr bool, retHandler func(job Job, ret interface{}, err error)) {
+	if jobs == nil || len(jobs) <= 0 {
+		panic("job required")
+	}
+
+	mux := &sync.Mutex{}
+	hasErr := false
+
+	sem := make(chan bool, concurrency)
+	for _, job := range jobs {
+		mux.Lock()
+		if hasErr && stopOnErr {
+			mux.Unlock()
+			break
+		}
+		mux.Unlock()
+
+		sem <- true
+		go func(j Job) {
+			defer func() { <-sem }()
+			ret, err := j.Run()
+			if err != nil {
+				mux.Lock()
+				hasErr = true
+				mux.Unlock()
+			}
+			retHandler(j, ret, err)
+		}(job)
+	}
+
+	for i := 0; i < concurrency; i++ {
+		sem <- true
+	}
+}
